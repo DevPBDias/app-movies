a6ba77ad0b31c0b54657fb5d601190c5510fa564
DevPBDias <devpbdias@gmail.com>
2023-12-12 22:42:12 -0300

    feat: correct imports to make connection with db

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,12 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import connectDatabase from "./src/database/database.js";
-import router from "./src/routes/index.js";
+import connectDatabase from "./src/data/dataMongoDB.js";
 
 const app= express();
 
 connectDatabase();
 app.use(cors());
 app.use(express.json());
-app.use(router);
 
 export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import app from "./app";
+import app from "./app.js";
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3004;
 app.listen(port, () => console.log(`Server running on port: ${port}`));
