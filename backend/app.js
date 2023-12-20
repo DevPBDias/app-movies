a1f25299a0f8e1dda11173784527ee842d58e6c0
DevPBDias <devpbdias@gmail.com>
2023-12-19 23:50:43 -0300

    fix: swagger name route and added route to app.js

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ import connectDatabase from "./src/data/dataMongoDB.js";
 import userRoute from "./src/routes/user.route.js";
 import loginRoute from "./src/routes/login.route.js";
 import movieRoute from "./src/routes/movie.route.js";
+import swaggerRoute from "./src/routes/swagger.route.js";
 
 const app= express();
 
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use("/user", userRoute);
 app.use("/login", loginRoute);
 app.use("/movie", movieRoute);
+app.use("/documentation", swaggerRoute);
 
 
 export default app;
