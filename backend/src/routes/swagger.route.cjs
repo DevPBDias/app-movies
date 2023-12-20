a73623c8ba29c9803505b5e71f40f857c4360c12
DevPBDias <devpbdias@gmail.com>
2023-12-19 23:45:51 -0300

    fix:  swagger imports

--- backend/src/routes/swagger.route.cjs
+++ backend/src/routes/swagger.route.cjs
@@ -1,8 +1,8 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const router = require("express").Router();
+import { Router } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../../swagger.json'
 
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("../swagger.json");
+const router = Router();
 
 router.use("/", swaggerUi.serve);
 router.get("/", swaggerUi.setup(swaggerDocument));
